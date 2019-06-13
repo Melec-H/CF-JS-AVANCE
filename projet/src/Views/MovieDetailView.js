38e4f71467d98ce1a106a388ed10b1f2b5a39a94
Cédric <cedric.leprohon@yahoo.fr>
2019-06-13 17:08:05 +0200

    je push

--- projet/src/Views/MovieDetailView.js
+++ projet/src/Views/MovieDetailView.js
@@ -2,26 +2,50 @@ import React, {Component} from "react";
 import Container from '@material-ui/core/Container';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
+import {getFilmDetailFromApi} from "../API/TMDB";
 
 
 export class MovieDetailView extends Component {
 
+    state = {
+        title: null,
+        genres: null,
+        overview: null,
+        poster_path: null,
+        production_companies: null,
+        release_date: null
+    }
+
+    componentDidMount() {
+        this.getFilm(this.props.match.params.movieId);
+    }
+
+    async getFilm(id) {
+        const response = await getFilmDetailFromApi(id);
+
+        this.setState(response);
+        return response;
+
+    }
+
     render() {
-        return(
+        console.log(this.state);
+        const { title, genres, overview, poster_path, production_companies, release_date } = this.state;
+        return (
             <Container fixed>
                 <Grid container spacing={3} direction="column">
                     <Grid item xs={12}>
-                        <h1>{this.props.match.params.movieId}</h1>
+                        <h1>{title}</h1>
                     </Grid>
                     <Grid container direction="row">
-                        <Grid item xs direction="row">
-                            <img src="https://placekitten.com/250/350"  />
+                        <Grid item xs>
+                            <img src="https://placekitten.com/250/350"/>
                         </Grid>
-                        <Grid xs={9} >
+                        <Grid container xs={9}>
                             <Grid container spacing={3} direction="column">
                                 <Grid>
                                     <Grid container spacing={3} direction="row">
-                                        <Grid  style={{
+                                        <Grid style={{
                                             margin: 10
                                         }}>
                                             <Paper style={{
@@ -30,7 +54,7 @@ export class MovieDetailView extends Component {
                                                 Horreur/Frisson/Thriller
                                             </Paper>
                                         </Grid>
-                                        <Grid  style={{
+                                        <Grid style={{
                                             margin: 10
                                         }}>
                                             <Paper style={{
@@ -39,10 +63,10 @@ export class MovieDetailView extends Component {
                                                 Release Date: 2010
                                             </Paper>
                                         </Grid>
-                                        <Grid  style={{
+                                        <Grid style={{
                                             margin: 10
                                         }}>
-                                            <Paper  style={{
+                                            <Paper style={{
                                                 padding: 5
                                             }}>
                                                 Lion's Gate / HBO
@@ -51,11 +75,7 @@ export class MovieDetailView extends Component {
                                         <Grid>
                                             <Paper style={{
                                                 padding: 10
-                                            }}>I guess if you want children beaten, you have to do it yourself. I just told you! You've killed me! And so we say goodbye to our beloved pet, Nibbler, who's gone to a place where I, too, hope one day to go. The toilet.
-                                                <p>Guards! Bring me the forms I need to fill out to have her taken away! Fry! Quit doing the right thing, you jerk! Fetal stemcells, aren't those controversial? Why, those are the Grunka-Lunkas! They work here in the Slurm factory.</p>
-                                                <p>No. We're on the top. Fry! Stay back! He's too powerful! I've got to find a way to escape the horrible ravages of youth. Suddenly, I'm going to the bathroom like clockwork, every three hours. And those jerks at Social Security stopped sending me checks. Now 'I'' have to pay ''them'!</p>
-                                                <p>I saw you with those two "ladies of the evening" at Elzars. Explain that. But, like most politicians, he promised more than he could deliver. Say what? Oh Leela! You're the only person I could turn to; you're the only person who ever loved me.</p>
-                                                <p>Yes. You gave me a dollar and some candy. Now, now. Perfectly symmetrical violence never solved anything. You mean while I'm sleeping in it? Shut up and take my money! Are you crazy? I can't swallow that.</p></Paper>
+                                            }}>{overview}</Paper>
                                         </Grid>
                                     </Grid>
                                 </Grid>
--- projet/src/Views/MoviesView.js
+++ projet/src/Views/MoviesView.js
@@ -2,6 +2,10 @@ import React, {Component} from "react";
 
 
 export class MoviesView extends Component {
+    componentDidMount() {
+
+    }
+
     render() {
         return  ('MoviesPage')
     }
